c29c297df7efa9bbeeececbbcf4b78658e650930
zelei.fan <1156447105@qq.com>
2017-06-16 18:19:20 +0800

    update

--- pony_service/common/src/main/java/com/pony/common/jvm/JvmTest.java
+++ pony_service/common/src/main/java/com/pony/common/jvm/JvmTest.java
@@ -1,5 +1,8 @@
 package com.pony.common.jvm;
 
+import com.beust.jcommander.internal.Lists;
+
+import java.util.List;
 import java.util.Vector;
 
 /**
@@ -54,11 +57,84 @@ public class JvmTest {
         int d = 2;
 
         /*包装类：Integer,Double等，
-        * 基本类型的定义都是存在栈中，而包赚类型是普通对象，数据存放在堆中
+        * 基本类型的定义都是存在栈中，而包装类型是普通对象，数据存放在堆中
         * */
         int e = 3;
         Integer f = new Integer(3);
-        System.out.println(e == f);
+        System.out.println(e == f);/*结果：true；当int类型和integer比较时，integer会自动拆箱成int类型，所以e，f比较的是int类型的值*/
 
+        /*下面的例子结果是因为integer中默认的吧常用数字区间-128~127之间的数字缓存起来，
+        * 所以当两个integer（注意是没有new的情况下）比较时在这区间内的返回的是true，而大于这个范围的返回的是false
+        *
+        * new的对象肯定是不同，与缓存的区间无关，因为是重新开辟的空间
+        * */
+        Integer g = 100;
+        Integer h = 100;
+        Integer g1 = 150;
+        Integer h1 = 150;
+        System.out.println(g == h);/*true*/
+        System.out.println(g1 == h1);/*false*/
+
+        Integer g2 = new Integer(50);
+        Integer h2 = new Integer(50);
+        System.out.println(g2 == h2);/*false*/
+
+        /*String是比较特殊的包装类，它并不属于基本类型
+        * */
+        String i = "abc";
+        String j = "abc";
+        String k = new String("abc");
+        String l = new String("abc");
+        System.out.println(i == j);/*true: String创建的常量都存放在常量池中*/
+        System.out.println(j == k);/*false: new出来的都是独立的一个，string的是存在栈中的，不共享数据*/
+        System.out.println(k == l);/*false：*/
+
+        /*final,修饰的变量不变，引用本身的不变和引用指向不变;
+        * 由下面的例子可见，final只是对地址有效，迫使引用只能指向那个对象，但是对象里面值得变化并不管
+        *
+        * 注意：final修饰的类变量（成员变量）必须赋初始值
+        * */
+        final StringBuffer m = new StringBuffer("immutable");
+        final StringBuffer n = new StringBuffer("not immutable");
+        /*m=n;//编译期错误*/
+        n.append("qqq");/*编译成功，引用指向不变，n指向的还是new StringBuffer("not immutable")这个对象，只是把对象中的内容改变了*/
+        String m1 = "qq";
+        m1 = m1 + "ww";/*这里m1其实是重新指向到数据为qqww的这个指向，而原来的qq就不被引用，这样会浪费内存空间*/
+        final List<Integer> list = Lists.newArrayList();
+        list.add(1);
+
+        /*
+        * 初始化不同类型对象需要花费的时间：
+        *   本地赋值：i = n; 1.0
+        *   实例赋值：this.i = n; 1.2
+        *   方法调用：func(); 5.9
+        *   新建对象：New Object(); 980
+        *   新建数组：New int[10]; 3100
+        * */
       }
+}
+
+class Dog {
+    Dog c;
+    String name;
+    //1.main()方法位于栈上
+    public static void main(String[] args) {
+    //2.在栈上创建引用变量d,但Dog对象尚未存在
+        Dog d;
+    //3.创建新的Dog对象，并将其赋予d引用变量
+        d = new Dog();
+    //4.将引用变量的一个副本传递给go()方法
+        d.go(d);
+    }
+    //5.将go()方法置于栈上，并将dog参数作为局部变量
+    void go(Dog dog){
+    //6.在堆上创建新的Collar对象，并将其赋予Dog的实例变量
+        c = new Dog();
+    }
+    //7.将setName()添加到栈上，并将dogName参数作为其局部变量
+    void setName(String dogName){
+    //8.name的实例对象也引用String对象
+        name =dogName;
+    }
+    //9.程序执行完成后，setName()将会完成并从栈中清除，此时，局部变量dogName也会消失，尽管它所引用的String仍在堆上
 }
